56bf208e434f49092dfa055db0f68fb4f2f0f1be
Satyam Banwale <satyambanwale786@gmail.com>
2025-04-29 23:44:31 +0530

    added mongodb uri from .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest-auth'),
+    ConfigModule.forRoot({
+      isGlobal: true, // ✅ makes config accessible app-wide
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
     AuthModule,
     UserModule,
   ],
