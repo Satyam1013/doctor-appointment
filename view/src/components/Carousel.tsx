d4946dcfc972245369b7b1d6a5dbcb7b13760bbb
Satyam Banwale <satyambanwale786@gmail.com>
2025-05-12 01:11:16 +0530

    fixed carousel

--- view/src/components/Carousel.tsx
+++ view/src/components/Carousel.tsx
@@ -4,12 +4,16 @@ import {
   StyleSheet,
   ScrollView,
   ImageSourcePropType,
+  Dimensions,
 } from 'react-native';
 
 interface CarouselProps {
   images: ImageSourcePropType[];
 }
 
+const { width: screenWidth } = Dimensions.get('window');
+
+const imageWidth = screenWidth * 0.9;
 export default function Carousel({ images }: CarouselProps) {
   return (
     <ScrollView
@@ -26,6 +30,11 @@ export default function Carousel({ images }: CarouselProps) {
 }
 
 const styles = StyleSheet.create({
-  container: { height: 150, marginBottom: 12 },
-  image: { width: 350, height: 150, borderRadius: 10, marginHorizontal: 6 },
+  container: { height: 150, marginBottom: 12, marginRight: 10, marginLeft: 10 },
+  image: {
+    width: imageWidth,
+    height: 150,
+    borderRadius: 10,
+    marginHorizontal: 6,
+  },
 });
