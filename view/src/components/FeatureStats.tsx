4349175904d41ab9ce8337bff812675c948355fa
Satyam Banwale <satyambanwale786@gmail.com>
2025-05-16 15:33:41 +0530

    added footer image

--- view/src/components/FeatureStats.tsx
+++ view/src/components/FeatureStats.tsx
@@ -6,36 +6,25 @@ import { View, Image, StyleSheet, Dimensions } from 'react-native';
 const screenWidth = Dimensions.get('window').width;
 
 export default function FeatureStats() {
-  const images = [
-    require('../../assets/images/features.png'),
-    require('../../assets/images/features2.png'),
-    require('../../assets/images/features3.png'),
-    require('../../assets/images/features4.png'),
-  ];
-
   return (
     <View style={styles.container}>
-      {images.map((img, idx) => (
-        <Image key={idx} source={img} style={styles.image} resizeMode="cover" />
-      ))}
+      <Image
+        source={require('../../assets/images/footer.jpeg')}
+        style={styles.image}
+        resizeMode="cover"
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 10,
     width: screenWidth,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    backgroundColor: '#E9F9FA', // same light blue background
-    padding: 16,
-    borderRadius: 12,
+    backgroundColor: '#E9F9FA',
   },
   image: {
-    width: (screenWidth - 48) / 2, // two columns with gap
-    height: 100,
-    borderRadius: 12,
-    marginBottom: 16,
+    width: screenWidth,
+    height: 180,
   },
 });
