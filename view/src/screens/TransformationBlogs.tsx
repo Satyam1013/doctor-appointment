6ac1f03d6f15d8a9202afc3994ad1089254205a7
Satyam Banwale <satyambanwale786@gmail.com>
2025-05-20 18:07:00 +0530

    added dynamic year

--- view/src/screens/TransformationBlogs.tsx
+++ view/src/screens/TransformationBlogs.tsx
@@ -4,26 +4,26 @@ import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
 
 export default function TransformationBlogDetailsScreen({ route }: any) {
   const { blog } = route.params;
-
+  const years = Math.floor(Math.random() * 14) + 2;
   return (
     <ScrollView style={styles.container}>
       <Image source={blog.img} style={styles.image} resizeMode="cover" />
 
       <View style={styles.details}>
         <Text>
-          For over 8 years, I struggled with severe lower back pain. Simple
-          tasks like standing for long periods or sitting at a desk became
-          unbearable. I tried physiotherapy, painkillers, and various modern
-          treatments but the relief was always temporary. After much research, I
-          consulted with an Ayurvedic doctor who designed a customized
-          Panchakarma therapy along with herbal supplements. The therapy not
-          only helped relieve my pain but also strengthened my muscles and
-          improved my posture. Within 4 months, I saw incredible improvement — I
-          could walk longer, stand comfortably, and even resume yoga, which I
-          had given up years ago. Today, I am pain-free and grateful for the
-          natural healing powers of Ayurveda. If you suffer from chronic back
-          pain, I highly recommend exploring Ayurvedic treatments for
-          long-lasting relief.`,
+          `For over ${years} years, I struggled with severe lower back pain.
+          Simple tasks like standing for long periods or sitting at a desk
+          became unbearable. I tried physiotherapy, painkillers, and various
+          modern treatments but the relief was always temporary. After much
+          research, I consulted with an Ayurvedic doctor who designed a
+          customized Panchakarma therapy along with herbal supplements. The
+          therapy not only helped relieve my pain but also strengthened my
+          muscles and improved my posture. Within 4 months, I saw incredible
+          improvement — I could walk longer, stand comfortably, and even resume
+          yoga, which I had given up years ago. Today, I am pain-free and
+          grateful for the natural healing powers of Ayurveda. If you suffer
+          from chronic back pain, I highly recommend exploring Ayurvedic
+          treatments for long-lasting relief.`,
         </Text>
       </View>
     </ScrollView>
